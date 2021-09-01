dcbf9619e8f5f1de6c19f5b81754816311c2eed7
godawajz <godawajz@g.cofc.edu>
2021-09-01 12:41:57 -0400

    Completed hw1

--- assignments/hw1/hello.py
+++ assignments/hw1/hello.py
@@ -1,7 +1,9 @@
-# File: hello.py
-"""hello.py
-Prints a simple \"hello, world!\" message."""
-# by Joanna Godawa
+"""
+Name: Joanna Godawa
+file: hello.py
+Prints a simple \"hello, world!\" message.
+
+I certify that this assignment is my own work."""
 
 def main():
     print("hello, world!")
